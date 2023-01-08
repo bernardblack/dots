012f85ade48245271e96acf0ed4c8f1b0246fd1e
Bernard <bernmbox@gmail.com>
2023-01-08 21:28:12 +0200

    knock knock

--- .config/i3/autorun.sh
+++ .config/i3/autorun.sh
@@ -1,5 +1,4 @@
 setxkbmap -layout us,ru,ua -variant -option grp:ctrl_shift_toggle,grp_led:scroll,terminate:ctrl_alt_bksp,compose:ralt
 xrdb ~/.Xresources
 nitrogen --restore
-picom
 xrandr --output eDP1 --auto --output DP1 --auto --right-of eDP1 --output HDMI1 --auto --left-of eDP1
